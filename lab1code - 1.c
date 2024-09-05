acd10dfe1d3243fb1052a7cbaa636d998a660d5f
ROYSONU <ROYKAMLESH1976@GMAIL.COM>
2024-09-05 16:26:20 +0530

    Create lab1code - 1.c

---
+++ lab1code - 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+    int c,f;
+    f=(9.0/5.0)*c + 32;
+    printf("enter c");
+    scanf("%d", &c);
+    printf("the value of f is %f", f=(9.0/5.0)*c+32);
+
+
+
+    return 0;}
+
